2c66786e07efbd401fe10d4f8c6eea99304de851
juniorCoderKrithika <72335062+juniorCoderKrithika@users.noreply.github.com>
2022-06-09 17:14:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+array_1=["hexagon","door","plane","farm","moon","panda"]
+random_no = Math.floor((Math.random()*array_1.length)+1)
+Element_of_array = array_1[random_no]
+timer_counter="0"
+timer_check=""
+drawn_sketch=""
+answer_holder=""
+score="0"
